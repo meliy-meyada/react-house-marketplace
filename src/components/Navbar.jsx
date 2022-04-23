07f5e89dd6ccf806876949fb52f41cc66e56cf66
Meyada <79361511+meliy-meyada@users.noreply.github.com>
2022-04-24 06:43:41 +0700

    Idea Link Pages useNavigate Hook react-router-dom

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,39 @@
+import { useNavigate, useLocation } from 'react-router-dom'
+import { ReactComponent as OfferIcon} from '../assets/svg/localOfferIcon.svg'
+import { ReactComponent as ExploreIcon} from '../assets/svg/exploreIcon.svg'
+import { ReactComponent as PersonOutlineIcon} from '../assets/svg/personOutlineIcon.svg'
+
+
+
+function Navbar() {
+    const navigate = useNavigate()
+    const  location = useLocation()
+
+    return (
+        <footer className='navbar'>
+            <div className="navbarNav">
+                <ul className="navbarListItems">
+                    <li className="navbarListItems" onClick={() => navigate
+                        ('/')}>
+                        <ExploreIcon fill='#2c2c2c' width='36px' height='36px' />
+                        <p>Explore</p>
+                        </li>
+                    <li className="navbarListItems" onClick={() => navigate
+                        ('/offers')}>
+                        <OfferIcon fill='#2c2c2c' width='36px' height='36px' />
+                        <p>Offer</p>
+                        </li>
+                    <li className="navbarListItems" onClick={() => navigate
+                        ('/profile')}>
+                        <PersonOutlineIcon fill='#2c2c2c' width='36px' height='36px' />
+                        <p>Profile</p>
+                        </li>
+                    </ul>
+                </div>
+        </footer>
+    )
+
+}
+    
+
+export default Navbar
