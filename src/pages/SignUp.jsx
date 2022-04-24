377bfce1d37c8a946025887695b458c0451fccfb
Meyada <79361511+meliy-meyada@users.noreply.github.com>
2022-04-24 10:08:37 +0700

    Create getAuth firebase and Fix bug Not Found firestore because i'm import failed. this bug '

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react"
 import { Link, useNavigate } from "react-router-dom"
+import { getAuth, createUserWithEmailAndPassword, updateProfile } from 'firebase/auth'
+import { db } from '../firebase.config'
 import {ReactComponent as ArrowRightIcon} from '../assets/svg/keyboardArrowRightIcon.svg'
 import  visibilityIcon from '../assets/svg/visibilityIcon.svg'
 
@@ -23,6 +25,25 @@ function SignUp() {
         }))
 
     }
+    const onSubmit = async (e) => {
+        e.preventDefault()
+
+        try {
+            const auth = getAuth()
+            
+            const userCredential = await createUserWithEmailAndPassword(auth, email, password)
+
+            const user = userCredential.user
+
+            updateProfile(auth.currentUser, {
+                displayName: name
+            })
+
+            navigate('/')
+        }   catch (error) {
+            console.log(error);
+        }
+    }
 
 
     return (
@@ -33,7 +54,7 @@ function SignUp() {
                         Welcome Back!
                     </p>
                 </header>
-                <form>
+                <form onSubmit={ onSubmit }>
                     <input
                         type="text"
                         className="nameInput"
