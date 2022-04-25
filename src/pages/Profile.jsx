5bfa23ddc5dd000e9f0b667d2e2f0c765657b2d6
Meyada <79361511+meliy-meyada@users.noreply.github.com>
2022-04-26 05:39:28 +0700

    Create Function change displayName and save to database

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,18 +1,98 @@
-import { useState, useEffect } from 'react'
-import { getAuth } from 'firebase/auth'
+import { useState } from 'react'
+import { getAuth, updateProfile } from 'firebase/auth'
+import { updateDoc, doc } from 'firebase/firestore'
+import { db } from '../firebase.config'
+import { useNavigate, Link } from 'react-router-dom'
+import { toast} from 'react-toastify'
+
 
 
 function Profile() {
-
-    const [user, setUser] = useState(null)
-
     const auth = getAuth()
-    useEffect(() => {
-        setUser(auth.currentUser);
-    }, [])
+    const [changeDetails, setChangeDetails] = useState(false)
+    const [formData, setFromData] = useState({
+        name:  auth.currentUser.displayName,
+        email:  auth.currentUser.email,
+    })
+    const { email, name } = formData
 
 
-    return user ? <h1>{user.displayName}</h1> : 'Not Logged In'
+    const navigate = useNavigate()
+
+    const onLogout = () => {
+        auth.signOut()
+        navigate('/')
+    }
+
+    const onSubmit = async () => {
+        try {
+            if (auth.currentUser.displayName !== name) {
+                // Update displayName in firebase
+                await updateProfile(auth.currentUser, {
+                    displayName: name,
+                })
+                // Update in fireStore
+                const userRef = doc(db, 'users', auth.currentUser.uid)
+                await updateDoc(userRef, {
+                    name
+                })
+            }
+        } catch (error) {
+            toast.error('ไม่สามารถอัปเดตรายละเอียดโปรไฟล์ได้')
+        }
+    }
+
+    const onChange = (e) => {
+        setFromData((prevState) => ({
+            ...prevState,
+            [e.target.id] : e.target.value,
+        }))
+    }
+
+
+
+    return (
+        <div className="profile">
+            <header className="profileHeader">
+                <p className="pageHeader">My Profile</p>
+                <button type='button' className='logOut' onClick={onLogout}>
+                    ออกจากระบบ
+                </button>
+            </header>
+            <main>
+                <div className="profileDetailsHeader">
+                    <p className="profileDetailsText">ข้อมูลส่วนตัว</p>
+                    <p className="changePersonalDetails" onClick={() => {
+                        changeDetails && onSubmit()
+                        setChangeDetails((prevState) => !prevState)
+                    }}>
+                        { changeDetails ? 'เสร็จแล้ว' : 'เปลี่ยน'}
+                    </p>
+                </div>
+                <div className="profileCard">
+                    <form>
+                        <input
+                            type="text"
+                            id="name"
+                            className={!changeDetails ? 'profileName' : 'profileNameActive'}
+                            disabled={!changeDetails}
+                            value={name}
+                            onChange={onChange}
+                        />
+                        <input
+                            type="text"
+                            id="email"
+                            className={!changeDetails ? 'profileEmail' : 'profileEmailActive'}
+                            disabled={!changeDetails}
+                            value={email}
+                            onChange={onChange}
+                        />
+                    </form>
+                </div>
+            </main>
+        </div>
+    
+    )
 }
 
 export default Profile
