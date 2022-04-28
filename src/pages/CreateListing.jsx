4cb47f610a0b1c1fee47eebb7ba3e993ec7adb97
Meyada <79361511+meliy-meyada@users.noreply.github.com>
2022-04-28 13:00:08 +0700

    Add Function map geocoding API in CreatListing Address

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect, useRef } from 'react'
 import { getAuth, onAuthStateChanged } from 'firebase/auth'
 import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
 import Spinner from '../components/Spinner'
 
 function CreateListing() {
@@ -59,9 +60,53 @@ function CreateListing() {
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [isMounted])
 
-    const onSubmit = (e) => {
+    const onSubmit = async (e) => {
         e.preventDefault()
 
+        setLoading(true)
+
+        if (discountedPrice >= regularPrice) {
+        setLoading(false)
+        toast.error('Discounted price needs to be less than regular price')
+        return
+        }
+
+        if (images.length > 6) {
+        setLoading(false)
+        toast.error('Max 6 images')
+        return
+        }
+
+        let geolocation = {}
+        let location
+
+        if (geolocationEnabled) {
+        const response = await fetch(
+            `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_GEOCODE_API_KEY}`
+        )
+
+        const data = await response.json()
+
+        geolocation.lat = data.results[0]?.geometry.location.lat ?? 0
+        geolocation.lng = data.results[0]?.geometry.location.lng ?? 0
+
+        location =
+            data.status === 'ZERO_RESULTS'
+            ? undefined
+            : data.results[0]?.formatted_address
+
+        if (location === undefined || location.includes('undefined')) {
+            setLoading(false)
+            toast.error('Please enter a correct address')
+            return
+        }
+        } else {
+        geolocation.lat = latitude
+        geolocation.lng = longitude
+        location = address
+        }
+
+        setLoading(false)
     }
 
     const onMutate = (e) => {
@@ -291,7 +336,7 @@ function CreateListing() {
                 max='750000000'
                 required
                 />
-                {type === 'rent' && <p className='formPriceText'>฿ / Month</p>}
+                {type === 'rent' && <p className='formPriceText'>$ / Month</p>}
             </div>
 
             {offer && (
@@ -331,6 +376,6 @@ function CreateListing() {
         </main>
         </div>
     )
-}
+    }
 
 export default CreateListing
