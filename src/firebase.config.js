44b104b1d01e16338a1e9959b0261c10d6dff912
Meyada <79361511+meliy-meyada@users.noreply.github.com>
2022-04-28 19:30:42 +0700

    new_env

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,5 +1,7 @@
-import {initializeApp} from "firebase/app";
-import {getFirestore} from "firebase/firestore"
+import { initializeApp } from "firebase/app";
+import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
+import { connectAuthEmulator, getAuth } from "firebase/auth";
+import { getStorage, connectStorageEmulator } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -12,5 +14,14 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-initializeApp(firebaseConfig)
-export const db = getFirestore()
+initializeApp(firebaseConfig);
+
+export const db = getFirestore();
+export const auth = getAuth();
+const storage = getStorage();
+
+if (process.env.NODE_ENV !== "production") {
+    connectFirestoreEmulator(db, "localhost", 8080);
+    connectAuthEmulator(auth, "http://localhost:9099");
+    connectStorageEmulator(storage, "localhost", 9199);
+}
