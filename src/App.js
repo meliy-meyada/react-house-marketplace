ef24f3f59bf0d1c4d0ff4a1a783f2076592c7ec1
Meyada <79361511+meliy-meyada@users.noreply.github.com>
2022-04-30 17:30:23 +0700

    Create Map Listing page and Contact Landlord

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import SignUp from './pages/SignUp'
 import ForgotPassword from './pages/ForgotPassword'
 import CreateListing from "./pages/CreateListing"
 import Listing from "./pages/Listing"
+import Contact from "./pages/Contact"
 
   function App() {
     return (
@@ -28,7 +29,8 @@ import Listing from "./pages/Listing"
             <Route path="/sign-up" element={ <SignUp />} />
             <Route path="/forgot-password" element={ <ForgotPassword />} />
             <Route path="/create-listing" element={<CreateListing />} />
-            <Route path="/category/:categoryName/:listingId" element={ < Listing />} />
+            <Route path="/category/:categoryName/:listingId" element={< Listing />} />
+            <Route path="/contact/:landlordId" element={ < Contact />} />
           </Routes>
           <Navbar />
         </Router>
--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -1,17 +1,16 @@
-import { useState, useEffect } from "react"
-import { useParams, useNavigate, Link } from "react-router-dom"
-import { getDoc, doc } from "firebase/firestore"
-import { getAuth } from "firebase/auth"
-import { db } from "../firebase.config"
-import Spinner from "../components/Spinner"
+import { useState, useEffect } from 'react'
+import { Link, useNavigate, useParams } from 'react-router-dom'
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+import { getDoc, doc } from 'firebase/firestore'
+import { getAuth } from 'firebase/auth'
+import { db } from '../firebase.config'
+import Spinner from '../components/Spinner'
 import shareIcon from '../assets/svg/shareIcon.svg'
-
 
 function Listing() {
     const [listing, setListing] = useState(null)
     const [loading, setLoading] = useState(true)
     const [shareLinkCopied, setShareLinkCopied] = useState(false)
-    
 
     const navigate = useNavigate()
     const params = useParams()
@@ -19,23 +18,111 @@ function Listing() {
 
     useEffect(() => {
         const fetchListing = async () => {
-            const docRef = doc(db, 'listings', params.listingId)
-            const docSnap = await getDoc(docRef)
+        const docRef = doc(db, 'listings', params.listingId)
+        const docSnap = await getDoc(docRef)
 
-            if (docSnap.exists()) {
-                console.log(docSnap.data())
-                setListing(docSnap.data())
-
-                setLoading(false)
-            }
-
+        if (docSnap.exists()) {
+            setListing(docSnap.data())
+            setLoading(false)
         }
+        }
+
         fetchListing()
     }, [navigate, params.listingId])
 
+    if (loading) {
+        return <Spinner />
+    }
+
     return (
-        <div>Listing</div>
+        <main>
+        {/* SLIDER */}
+
+        <div
+            className='shareIconDiv'
+            onClick={() => {
+            navigator.clipboard.writeText(window.location.href)
+            setShareLinkCopied(true)
+            setTimeout(() => {
+                setShareLinkCopied(false)
+            }, 2000)
+            }}
+        >
+            <img src={shareIcon} alt='' />
+        </div>
+
+        {shareLinkCopied && <p className='linkCopied'>Link Copied!</p>}
+
+        <div className='listingDetails'>
+            <p className='listingName'>
+            {listing.name} - $
+            {listing.offer
+                ? listing.discountedPrice
+                    .toString()
+                    .replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+                : listing.regularPrice
+                    .toString()
+                    .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+            </p>
+            <p className='listingLocation'>{listing.location}</p>
+            <p className='listingType'>
+            For {listing.type === 'rent' ? 'Rent' : 'Sale'}
+            </p>
+            {listing.offer && (
+            <p className='discountPrice'>
+                ${listing.regularPrice - listing.discountedPrice} discount
+            </p>
+            )}
+
+            <ul className='listingDetailsList'>
+            <li>
+                {listing.bedrooms > 1
+                ? `${listing.bedrooms} Bedrooms`
+                : '1 Bedroom'}
+            </li>
+            <li>
+                {listing.bathrooms > 1
+                ? `${listing.bathrooms} Bathrooms`
+                : '1 Bathroom'}
+            </li>
+            <li>{listing.parking && 'Parking Spot'}</li>
+            <li>{listing.furnished && 'Furnished'}</li>
+            </ul>
+
+            <p className='listingLocationTitle'>Location</p>
+
+            <div className='leafletContainer'>
+            <MapContainer
+                style={{ height: '100%', width: '100%' }}
+                center={[listing.geolocation.lat, listing.geolocation.lng]}
+                zoom={13}
+                scrollWheelZoom={false}
+            >
+                <TileLayer attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url='https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png'
+                />
+
+                <Marker
+                position={[listing.geolocation.lat, listing.geolocation.lng]}
+                >
+                <Popup>{listing.location}</Popup>
+                </Marker>
+            </MapContainer>
+        </div>
+
+            {auth.currentUser?.uid !== listing.userRef && (
+            <Link
+                to={`/contact/${listing.userRef}?listingName=${listing.name}`}
+                className='primaryButton'
+            >
+            Contact Landlord
+            </Link>
+            )}
+        </div>
+        </main>
     )
 }
 
 export default Listing
+
+
